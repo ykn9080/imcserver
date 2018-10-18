720cca892de2bde62ff7709e7badcd6a8dad6da6
ykn9080 <youngkinam@gmail.com>
2018-10-18 18:46:14 +0900

    push for openshift, server_port and server_ip_address

--- server.js
+++ server.js
@@ -56,11 +56,22 @@ app.use(flash());
 
 require('./router/index.js')(app,passport);
 
-app.set('port', process.env.PORT || 3001);
+// app.set('port', process.env.PORT || 3001);
+// var server=app.listen(app.get('port'), function(){
+//   console.log('Server is running on Port: ',server.address().port);
+// });
 
-var server=app.listen(app.get('port'), function(){
-  console.log('Server is running on Port: ',server.address().port);
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
+
+
+
+
 
 // var server = app.listen(3001, function(){
 //     console.log("Express server has started on port 3001")
