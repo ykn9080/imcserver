5241f104df45100a5e9ebb73e6e4963d6b2b4bf0
ykn9080 <youngkinam@gmail.com>
2018-10-19 14:16:13 +0900

    fixup server.js for openshift server->app

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ require('./router/index.js')(app,passport);
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-server.listen(server_port, server_ip_address, function () {
+app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
 
