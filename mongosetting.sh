c38c5d3b4eb4aa96a863b4f4079f4ed7e5e9a1be
yknam <youngkinam@gmail.com>
2019-07-30 13:07:26 +0900

    add mongosetting.sh

--- mongosetting.sh
+++ mongosetting.sh
@@ -1,20 +1,10 @@
-#! /bin/bash
+#! /bin/sh
 
 echo "Step1: Change mongodb source"
-ipa=$(hostname)
+ipa=$(1)
 
 case "$ipa" in
 imcserver) sed -i 's+datasrc:basic.mongodb.local+datasrc:basic.mongodb.docean+g' config/index.js;;
 namubuntu) sed -i 's+datasrc:basic.mongodb.local+datasrc:basic.mongodb.namubuntu+g' config/index.js;;
 *) a="not registerd system!!";;
 esac
-
-echo "Step2: Create Volumes"
-arr=("mongodbdata" "jsondata" "svrnode_modules")
-for var in "${arr[@]}"
-do
-  docker volume create  $var
-done
-
-echo "Step3: docker-compose up"
-docker-compose up --build -d 
