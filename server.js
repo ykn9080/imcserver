cbcabbdd3be5914e6f811bd6c1361f590dda676b
ykn9080 <ykn9080@empal.com>
2020-03-06 18:51:55 +0900

    server.js include graphql,apollo

--- server.js
+++ server.js
@@ -2,18 +2,36 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var morgan=require('morgan');
+var morgan = require('morgan');
 var cors = require('cors');
+//Apollo, graphql setting
+const { ApolloServer, gql } = require("apollo-server-express");
+var { makeExecutableSchema } = require("graphql-tools");
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+//const schema = require('./graphql');
+//const apServer = new ApolloServer({ typeDefs, resolvers });
+console.log('this is resolver', resolvers)
+const apServer = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+apServer.applyMiddleware({ app });
+
 app.use(cors())
 
 
 //app.use(cookieParser());
 //app.set('router', __dirname + '/router/main');
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname+ '/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.json({limit: '50mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(bodyParser.json({ limit: '50mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
 app.use(morgan("combined"));
 
@@ -23,7 +41,7 @@ app.engine('html', require('ejs').renderFile);
 //Set up default mongoose connection
 const config = require('./config');
 //var mongoDB='mongodb://yknam:ykn9080@ds135399.mlab.com:35399/imcdb';
-mongoose.connect(config.currentsetting.datasrc,{ useNewUrlParser: true });
+mongoose.connect(config.currentsetting.datasrc, { useNewUrlParser: true });
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
 //Get the default connection
@@ -38,13 +56,13 @@ require('./database/mssql.js');
 
 var expressSession = require('express-session');
 app.use(expressSession({
-  secret: 'mySecretKey',
-  resave: false,
-  saveUninitialized: false
+    secret: 'mySecretKey',
+    resave: false,
+    saveUninitialized: false
 }));
 
- var passport = require('passport');
- //var LocalStrategy = require('passport-local').Strategy;
+var passport = require('passport');
+//var LocalStrategy = require('passport-local').Strategy;
 //  const jwtStrategry  = require("./strategies/jwt")
 // passport.use(jwtStrategry);
 
@@ -59,12 +77,12 @@ var flash = require('connect-flash');
 app.use(flash());
 require('./api/googleapis');
 
-require('./router/index.js')(app,passport);
+require('./router/index.js')(app, passport);
 
 
 app.set('port', process.env.PORT || 3001);
-var server=app.listen(app.get('port'), function(){
-  console.log('Server is running on Port!!: ',server.address().port);
+var server = app.listen(app.get('port'), function() {
+    console.log('Server is running on Port!!: ', server.address().port);
 });
 
 // //this is for openshift
